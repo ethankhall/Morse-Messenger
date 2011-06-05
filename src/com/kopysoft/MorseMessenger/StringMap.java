71c17830e6a89501145e0a908bc7dc5f0883bb72
Ethan Hall <ethan@kopysoft.com>
2011-06-05 12:38:59 -0500

    Added comments and the license file to the top of the file

--- src/com/kopysoft/MorseMessenger/StringMap.java
+++ src/com/kopysoft/MorseMessenger/StringMap.java
@@ -1,8 +1,35 @@
 package com.kopysoft.MorseMessenger;
 
+/**
+ * 			Copyright (C) 2011 by Ethan Hall
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ * 	in the Software without restriction, including without limitation the rights
+ * 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * 	copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *  
+ */
+
 import java.util.HashMap;
 import java.util.Map;
 
+/** Holds a dictionary of all the Morse Code translations
+ * 
+ * @author Ethan Hall
+ */
 public class StringMap {
 
 	Map<String, String> m = new HashMap<String, String>();
@@ -61,10 +88,28 @@ public class StringMap {
         m.put("\"", ".-..-.");
         m.put("$", "...-..-");
         m.put("@", ".--.-.");
+        m.put(" ", " ");
 	}
 	
+	/** Description of getVal(String value)
+	 * 
+	 * @param value	A single character string to get the Morse Code translation of 
+	 * @return A string that is the translation of Morse Code, if not found the value is null
+	 */
 	public String getVal(String value){
 		if(m.containsKey(value))
+			return m.get(value);
+		else
+			return null;
+	}
+	
+	/** Description of getVal(char value)
+	 * 
+	 * @param value	A single character to get the Morse Code translation of 
+	 * @return A string that is the translation of Morse Code, if not found the value is null
+	 */
+	public String getVal(char value){
+		if(m.containsKey(Character.toString(value)))
 			return m.get(value);
 		else
 			return null;
